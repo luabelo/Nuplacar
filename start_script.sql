205a9e1afa8051dc8be8fc1acada3467885df513
Matheus Cabrini <matheusbarraboni@gmail.com>
2022-11-27 23:01:36 -0300

    script database atualizado

--- start_script.sql
+++ start_script.sql
@@ -2,36 +2,53 @@ CREATE DATABASE IF NOT EXISTS nuplacar;
 
 USE nuplacar;
 
-CREATE TABLE usuario (
-	id_usuario int primary key auto_increment,
-	isAdm bit,
+CREATE TABLE tb_usuario (
+	idUsuario int primary key auto_increment,
 	nome varchar(200),
-	senha varchar(20)
-);
+	senha varchar(200),
+	tipoUsuario varchar(200));
 
-CREATE TABLE time (
-	nome varchar(30) primary key,
-	bandeira varchar(30) not null,
-	grupo char
-);
+INSERT INTO tb_usuario (nome, senha, tipoUsuario) 
+VALUES ('admin', 'admin', 'Administrador');
 
-CREATE TABLE classificacao (
-	nome varchar(30) primary key,
+CREATE TABLE tb_times (
+	idTime int primary key auto_increment,
+	nome varchar(20),
+	bandeira varchar(20),
 	pontos int,
 	jogos int,
 	vitorias int,
 	empates int,
 	derrotas int,
 	golPro int,
-	golContra int
+	golContra int,
+	saldoGols int
 );
 
-CREATE TABLE partida (
-	id_partida int primary key auto_increment,
-	time1 varchar(30) not null,
-	time2 varchar(30) not null,
-	gols_time1 int,
-	gols_time2 int
-);
+CREATE TABLE tb_grupo (
+	idGrupo char primary key,
+	time1 varchar(20),
+	time2 varchar(20),
+	time3 varchar(20),
+	time4 varchar(20));
 
-INSERT INTO usuario (nome, senha, isAdm) VALUES ('admin', 'admin', 1);
+
+INSERT INTO tb_times (nome) VALUES 
+('Qatar'), ('Equador'), ('Senegal'), ('Holanda'), 
+('Inglaterra'), ('Irã'), ('EUA'), ('País de Gales'),
+('Argentina'), ('Arábia Saudita'), ('México'), ('Polônia'),
+('França'), ('Dinamarca'), ('Tunísia'), ('Austrália'),
+('Espanha'), ('Alemanha'), ('Japão'), ('Costa Rica'),
+('Bélgica'), ('Canadá'), ('Marrocos'), ('Croácia'),
+('Brasil'), ('Sérvia'), ('Suíça'), ('Camarões'),
+('Portugal'), ('Gana'), ('Uruguai'), ('Coreia do Sul');
+
+INSERT INTO tb_grupos (idGrupo, time1, time2, time3, time4) VALUES
+('A', 'Qatar', 'Equador', 'Senegal', 'Holanda'),
+('B', 'Inglaterra', 'Irã', 'EUA', 'País de Gales'),
+('C', 'Argentina', 'Arábia Saudita', 'México', 'Polônia'),
+('D', 'França', 'Dinamarca', 'Tunísia', 'Austrália'),
+('E', 'Espanha', 'Alemanha', 'Japão', 'Costa Rica'),
+('F', 'Bélgica', 'Canadá', 'Marrocos', 'Croácia'),
+('G', 'Brasil', 'Sérvia', 'Suíça', 'Camarões'),
+('H', 'Portugal', 'Gana', 'Uruguai', 'Coreia do Sul');
